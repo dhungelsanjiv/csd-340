0079f65e28b9aeb8fc3c28e78ba34906fb920dfa
Sanjiv Dhungel <sadhungel@my365.bellevue.edu>
2022-02-27 01:06:17 -0600

    assignment-11.2.html is added.

---
+++ module-7/module7.3/Practice.java
@@ -0,0 +1,11 @@
+
+public class Practice {
+    public static void main(String arg[]) {
+        int x = 32;
+        double y = 36.75;
+
+        System.out.println("x mod 10 = " + x % 10);// 2
+        System.out.println("y mod 10 = " + y % 10);// 6.75
+
+    }
+}
